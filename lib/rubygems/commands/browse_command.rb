94e67d81586807c3a829b2c3a0a56d08339c5dde
Tim Pope <code@tpope.net>
2011-08-28 22:23:13 -0400

    Fall back to rubygems.org page

--- lib/rubygems/commands/browse_command.rb
+++ lib/rubygems/commands/browse_command.rb
@@ -14,9 +14,8 @@ class Gem::Commands::BrowseCommand < Gem::Browse::Command
       rescue Gem::LoadError
         get_json(name)[/"homepage_uri":\s*"([^"]*)"/, 1]
       end
-    if homepage.to_s.empty?
-      alert_warning "Gem '#{name}' lacks a homepage."
-    elsif !system('git', 'web--browse', homepage)
+    homepage = "http://rubygems.org/gems/#{name}" if homepage.to_s.empty?
+    unless system('git', 'web--browse', homepage)
       alert_error('Error starting web browser (using git web--browse).')
       terminate_interaction 1
     end
